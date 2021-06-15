0154f073598d317d96db8b1bbdd84b3775683832
Luciano Iam <oss@lucianoiam.com>
2021-06-16 00:35:23 +0200

    Update to dpf-webui 468f992

--- src/ui/castello-rev-ui.js
+++ src/ui/castello-rev-ui.js
@@ -16,7 +16,7 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-class CastelloRevUI extends WebUI {
+class CastelloRevUI extends DISTRHO_WebUI {
 
     constructor() {
         super();
