48c056bf23e327c9a4c7aca7960c07098d4de84a
Luciano Iam <oss@lucianoiam.com>
2021-06-15 23:55:21 +0200

    Add a script for updating dpf-webui
    Assuming it located in a local directory at the same level as this repo

---
+++ scripts/update-dpf-webui.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+DPF_WEBUI=../dpf-webui
+
+if [ ! -d $DPF_WEBUI ]; then
+   echo Could not find dpf-webui
+   exit 1
+fi
+
+cp -r $DPF_WEBUI/src/arch src/
+cp -r $DPF_WEBUI/src/base src/
+cp $DPF_WEBUI/Makefile.base.mk .
+cp $DPF_WEBUI/Makefile.plugins.mk .
+cp $DPF_WEBUI/Makefile.support.mk .
+
