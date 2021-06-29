261ccfa68dc7cc5e06babfde680d9bd6414107a7
Luciano Iam <oss@lucianoiam.com>
2021-06-29 19:28:01 +0200

    Update dependencies script

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 VST/LV2 reverb plugin featuring HTML UI and DSP algorithm from Csound
 ---------------------------------------------------------------------
 
+Based on [dpf-webui](https://github.com/lucianoiam/dpf-webui)
+
 This is work in progress...
--- scripts/update-deps.sh
+++ scripts/update-deps.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 DPF_WEBUI=../dpf-webui
+AWWW=../awww
 
 if [ ! -d $DPF_WEBUI ]; then
    echo Could not find source directory
@@ -10,8 +11,9 @@ fi
 cp -r $DPF_WEBUI/src/arch src/
 cp -r $DPF_WEBUI/src/base src/
 
-cp $DPF_WEBUI/src/ui/stub-webui.js src/ui/
-
 cp $DPF_WEBUI/Makefile.base.mk .
 cp $DPF_WEBUI/Makefile.plugins.mk .
 cp $DPF_WEBUI/Makefile.support.mk .
+
+cp $DPF_WEBUI/src/ui/stub-webui.js src/ui/
+cp $AWWW/awww.js src/ui/
