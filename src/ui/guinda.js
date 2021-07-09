13c2624d30d4c98234a09628c6d9337384c8ef6c
Luciano Iam <oss@lucianoiam.com>
2021-07-09 13:06:23 +0200

    Better mouse wheel implementation

--- src/ui/guinda.js
+++ src/ui/guinda.js
@@ -316,8 +316,10 @@ function ControlTrait() {
             dispatchControlStart(ev, ev.clientX, ev.clientY);
         }
 
-        dispatchControlContinue(ev, this._prevClientX - ev.deltaX,
-                                    this._prevClientY - ev.deltaY);
+        const k = ev.shiftKey ? 2 : 1;
+
+        dispatchControlContinue(ev, this._prevClientX + k * Math.sign(ev.deltaX),
+                                    this._prevClientY + k * Math.sign(ev.deltaY));
 
         if (this._controlTimeout) {
             clearTimeout(this._controlTimeout);
