d28fdab9c1637f2274fbb19cb8a561b1d2b9a281
Luciano Iam <oss@lucianoiam.com>
2021-07-12 10:26:54 +0200

    Update to apices 172c0ca

--- scripts/update-deps.sh
+++ scripts/update-deps.sh
@@ -3,8 +3,8 @@
 git submodule init
 git submodule update --recursive --remote
 
-git -C apices checkout master && git -C apices pull
-git -C guinda checkout master && git -C guinda pull
+git -C apices checkout master; git -C apices pull
+git -C guinda checkout master; git -C guinda pull
 
 cp apices/examples/webgain/ui/stub-ui.js src/ui/
 cp guinda/guinda.js src/ui/
