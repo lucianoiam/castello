c7f18a2d78b2306bd57771e43e927540276d20ec
Luciano Iam <oss@lucianoiam.com>
2021-07-12 10:30:38 +0200

    Fix deps script

--- scripts/update-deps.sh
+++ scripts/update-deps.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-git submodule init
-git submodule update --recursive --remote
-
 git -C apices checkout master; git -C apices pull
 git -C guinda checkout master; git -C guinda pull
+
+git submodule init
+git submodule update --recursive
 
 cp apices/examples/webgain/ui/stub-ui.js src/ui/
 cp guinda/guinda.js src/ui/
