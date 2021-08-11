46ecee2f8098dd3b17ec2b0a14b2fcf66b9fcfe7
Luciano Iam <oss@lucianoiam.com>
2021-08-11 10:42:52 +0200

    Reorganize some non-code files

--- scripts/update-deps.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-git -C hiphop checkout master; git -C hiphop pull
-git -C guinda checkout master; git -C guinda pull
-
-cp hiphop/examples/webgain/ui/stub-ui.js src/ui/
-cp guinda/guinda.js src/ui/
